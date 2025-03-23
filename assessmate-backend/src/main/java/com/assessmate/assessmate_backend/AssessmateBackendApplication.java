ea72123a5c9507f1ee1fd6f978099d3712488be8
hangs0821 <sacamayhannahgrace@gmail.com>
2025-03-24 07:17:46 +0800

    Add Spring Boot backend structure

---
+++ assessmate-backend/src/main/java/com/assessmate/assessmate_backend/AssessmateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.assessmate.assessmate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmateBackendApplication.class, args);
+	}
+
+}
