e6bcddbe34d3c22dff4261a15a57912e20be3d5a
hangs0821 <sacamayhannahgrace@gmail.com>
2025-04-20 17:49:56 +0800

    Update Backend with Oauth

---
+++ assessmate-backend/src/main/java/edu/cit/AssessMate/Utils/ClassIDGenerator.java
@@ -0,0 +1,32 @@
+package edu.cit.AssessMate.Utils;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ClassIDGenerator implements IdentifierGenerator {
+
+    @Override
+    public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
+        String prefix = "CLASS";
+        try (Connection connection = session.getJdbcConnectionAccess().obtainConnection();
+                Statement statement = connection.createStatement();
+                ResultSet rs = statement
+                        .executeQuery("SELECT MAX(CAST(SUBSTRING(class_id, 6) AS UNSIGNED)) FROM classes")) {
+
+            int nextId = 1;
+            if (rs.next() && rs.getString(1) != null) {
+                nextId = rs.getInt(1) + 1;
+            }
+            return prefix + String.format("%03d", nextId);
+        } catch (SQLException e) {
+            throw new HibernateException("Unable to generate Class ID", e);
+        }
+    }
+} 
